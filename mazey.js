b4015cfd5961ca15caa830f54d7dbabdceb5c54c
Ingo Schmied <ingo@ingoneering.com>
2020-07-18 10:50:29 -0700

    initial commit

---
+++ mazey.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('DOMContentLoaded', setCanvas);
+
+
+
+
+function setCanvas() {
+	const main = document.getElementById('main');
+	main.setAttribute('width', 200);
+	main.setAttribute('height', 200);	
+}
